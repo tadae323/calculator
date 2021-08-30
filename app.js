ff3416e04aedb5d1c444ba3c0dc3a42c9f91c631
Edyta <tadae@MacBook.local>
2021-08-30 21:08:25 +0200

    Created the logic

--- app.js
+++ app.js
@@ -1,15 +1,111 @@
-function add(number1, number2) {
-  return number1 + number2;
+/*  Variables */
+const numberKeys = document.querySelectorAll("[data-number]")
+const operatorKeys = document.querySelectorAll("[data-operation")
+const display = document.querySelector("#operation-window")
+const clearBtn = document.querySelector("#clear")
+const equal = document.querySelector("#equal")
+let previousOperand = ""
+let currentOperand = ""
+let operator = ""
+let operatorActive = false
+
+
+/* Event listeners */
+
+for (number of numberKeys) {
+  number.addEventListener("click", appendNumber)
 }
 
-function subtract(number1, number2) {
-  return number1 - number2;
+for (operatorKey of operatorKeys) {
+  operatorKey.addEventListener("click", updateOperator)
 }
 
-function multiply(number1, number2) {
-  return number1 * number2;
+clearBtn.addEventListener("click", clear)
+
+equal.addEventListener("click", operate)
+
+
+/* Functions */
+
+function add(num1, num2) {
+  let result = num1 + num2
+  return result
 }
 
-function divide(number1, number2) {
-  return number1 / number2;
+function subtract(num1, num2) {
+  let result = num1 - num2
+  return result
+}
+
+function multiply(num1, num2) {
+  let result = num1 * num2
+  return result
+}
+
+function divide(num1, num2) {
+  if (num2 === 0) {
+    alert("You can't divide by 0!")
+    clear()
+    return
+  } else {
+    let result = num1 / num2
+    return result
+  }
+}
+
+function updateOperator(operatorKey) {
+  operator = operatorKey.target.innerText
+  operatorActive = true
+  currentOperand = getDisplayNums()
+}
+
+function operate() {
+  if (currentOperand) {
+    previousOperand = currentOperand
+    currentOperand = getDisplayNums()
+  } 
+  display.innerText = calculateResult()
+  operatorActive = true
+}
+
+function calculateResult() {
+  let result
+  const prev = parseFloat(previousOperand)
+  const current = parseFloat(currentOperand)
+  switch (operator) {
+    case "+":
+      result = add(prev, current)
+      break
+    case "-":
+      result = subtract(prev, current)
+      break
+    case "×":
+      result = multiply(prev, current)
+      break
+    case "÷":
+      result = divide(prev, current)
+      break
+  }
+  return result
+}
+
+function appendNumber(e) {
+  if (display.innerText.length > 13) return
+  if (display.innerText === "0" || operatorActive === true) {
+    display.innerText = e.target.innerText
+    operatorActive = false
+  } else {
+    display.innerText += e.target.innerText
+  }
+}
+
+function getDisplayNums() {
+  return display.innerText
+}
+
+function clear() {
+  previousOperand = ""
+  currentOperand = ""
+  operator = ""
+  display.innerText = "0"
 }
